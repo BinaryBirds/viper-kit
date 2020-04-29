8be972d60e8b43e7ad85a827ecd7c3ced26b64e7
Tibor Bödecs <mail.tib@gmail.com>
2020-04-30 00:04:43 +0200

    Reverted to static & dynamic lib support

--- Package.swift
+++ Package.swift
@@ -7,7 +7,8 @@ let package = Package(
        .macOS(.v10_15)
     ],
     products: [
-        .library(name: "ViperKit", type: .dynamic, targets: ["ViperKit"]),
+        .library(name: "ViperKit", targets: ["ViperKit"]),
+        .library(name: "ViperKitDynamic", type: .dynamic, targets: ["ViperKit"]),
     ],
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "4.4.0"),
